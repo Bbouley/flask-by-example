2822e76fa920d966ab2aafd404c1dde416a51025
bbouley <b.bouley@hotmail.co.uk>
2016-03-04 17:58:04 -0700

    added in angular and backend worker process

--- app.py
+++ app.py
@@ -3,14 +3,15 @@ from flask.ext.sqlalchemy import SQLAlchemy
 from stop_words import stops
 from collections import Counter
 from bs4 import BeautifulSoup
+from rq import Queue
+from rq.job import Job
+from worker import conn
 import operator
 import os
 import requests
 import re
 import nltk
-from rq import Queue
-from rq.job import Job
-from worker import conn
+import json
 
 
 #################
@@ -33,18 +34,24 @@ from models import *
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    results = {}
-    if request.method == "POST":
-        # get url that the person has entered
-        url = request.form['url']
-        if 'http://' not in url[:7]:
-            url = 'http://' + url
-        job = q.enqueue_call(
-            func=count_and_save_words, args=(url,), result_ttl=5000
-        )
-        print(job.get_id())
+    return render_template('index.html')
 
-    return render_template('index.html', results=results)
+
+@app.route('/start', methods=['POST'])
+def get_counts():
+    # get url
+    data = json.loads(request.data.decode())
+    url = data["url"]
+    # form URL, id necessary
+    if 'http://' not in url[:7]:
+        url = 'http://' + url
+    # start job
+    job = q.enqueue_call(
+        func=count_and_save_words, args=(url,), result_ttl=5000
+    )
+    # return created job id
+    return job.get_id()
+
 
 @app.route("/results/<job_key>", methods=['GET'])
 def get_results(job_key):
