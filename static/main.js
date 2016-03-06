24b9dab74451d881a0ae1275554302abb33e2b32
bbouley <b.bouley@hotmail.co.uk>
2016-03-05 21:00:51 -0700

    ui update and gifs added

--- static/main.js
+++ static/main.js
@@ -5,20 +5,28 @@
   angular.module('WordcountApp', [])
 
     .controller('WordcountController', ['$scope', '$log', '$http', '$timeout', function($scope, $log, $http, $timeout) {
+    $scope.submitButtonText = "Submit";
+    $scope.loading = false;
+
+
     $scope.getResults = function() {
 
       // get the URL from the input
       var userInput = $scope.input_url;
       // fire the API request
-      $http.post('/start', {"url": userInput}).
-        success(function(results) {
-          $log.log(results);
-          getWordCount(results);
-        }).
-        error(function(error) {
-          $log.log(error);
-        });
-      };
+
+        $http.post('/start', {"url": userInput}).
+          success(function(results) {
+            $log.log(results);
+            getWordCount(results);
+            $scope.wordcounts = null;
+            $scope.loading = true;
+            $scope.submitButtonText = "Loading...";
+          }).
+          error(function(error) {
+            $log.log(error);
+          });
+        };
 
         function getWordCount(jobID) {
 
@@ -29,9 +37,11 @@
             $http.get('/results/'+jobID).
               success(function(data, status, headers, config) {
                 if(status === 202) {
-                  $log.log(data, status);
+                  $log.log(data, status)
                 } else if (status === 200){
                   $log.log(data);
+                  $scope.loading = false;
+                  $scope.submitButtonText = "Submit";
                   $scope.wordcounts = data;
                   $timeout.cancel(timeout);
                   return false;
